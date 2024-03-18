97dcf73a62a05612f9674b23ef4da0d7646e9e68
T.M. <ace071144@gmail.com>
2024-03-18 22:43:51 +0900

    development追加

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
 var openSpMenu = function() {
-  var overlay = document.querySelector(".sp-navOverlay");
+  var overlay = document.querySelector(".js-navOverlay");
   overlay.classList.add('visible');
-  var spNavBtn = document.querySelector(".sp-navButton");
+  var spNavBtn = document.querySelector(".js-navButton");
   spNavBtn.classList.add('sp-nav-open');
   document.body.classList.add('no_scroll');
 };
 
 var closeSpMenu = function() {
-  var overlay = document.querySelector(".sp-navOverlay");
+  var overlay = document.querySelector(".js-navOverlay");
   overlay.classList.remove('visible');
-  var spNavBtn = document.querySelector(".sp-navButton");
+  var spNavBtn = document.querySelector(".js-navButton");
   spNavBtn.classList.remove('sp-nav-open');
   document.body.classList.remove('no_scroll');
 };
 
-var spNavOpenButton = document.querySelector('.sp-navMenu');
+var spNavOpenButton = document.querySelector('.p-sp__navMenu');
 spNavOpenButton.addEventListener('click', openSpMenu, false);
 
-var spNavCloseButton = document.querySelector('.sp-navClose');
+var spNavCloseButton = document.querySelector('.p-sp__navClose');
 spNavCloseButton.addEventListener('click', closeSpMenu, false);
 
-var spCloseLinks = document.querySelectorAll('.sp-close-link');
+var spCloseLinks = document.querySelectorAll('.js-sp-closeLink');
 spCloseLinks.forEach(function(elem) {
   elem.addEventListener('click', closeSpMenu, false);
 });
